c199c3cc82d642d233cb4e57f911469b73123e5e
Maksym Cherniavskyi <morfey897@gmail.com>
2023-01-30 21:56:00 +0200

    Removed .env variables

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,9 +3,9 @@ const LANG_REG = /^(\/[a-z]{2})(?:\/|\?|$)/;
 export function getLang(href: string) {
   const url = new URL(href);
   const [, urlLang] = url.pathname.match(LANG_REG) || [];
-  if (!urlLang) return import.meta.env.DEFAULT_LANG;
+  if (!urlLang) return 'uk';
   const lang = urlLang.replace(/^\/|\/$/g, '');;
-  if (import.meta.env.AVAILABLE_LANG.split(",").map(l => l.trim()).filter(l => !!l && l.length).includes(lang)) return lang;
+  if ('en,uk,ru'.split(",").map(l => l.trim()).filter(l => !!l && l.length).includes(lang)) return lang;
   return undefined;
 }
 
@@ -13,6 +13,6 @@ export function normalize(href: string) {
   const url = new URL(href);
   const urlLang = getLang(href);
   const path = url.href.replace(url.origin, '');
-  const lang = urlLang === import.meta.env.DEFAULT_LANG ? '' : urlLang;
+  const lang = urlLang === 'uk' ? '' : urlLang;
   return `${url.protocol}//` + [url.host, urlLang ? path.replace(LANG_REG, '/' + lang + '/') : '/' + lang + '/' + path].join('/').replace(/\/{2,}/g, "/");
 }
