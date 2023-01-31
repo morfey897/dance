201d99902817e5c3e54cf8dfc043c7c71bcd7001
Maksym Cherniavskyi <morfey897@gmail.com>
2023-01-31 13:18:32 +0200

    Added data api

---
+++ src/pages/api/data.ts
@@ -0,0 +1,18 @@
+export async function get(params) {
+  // const id = params.id;
+  const product = {id: 'some_id'};//await getProduct(id);
+
+  if (!product) {
+    return new Response(null, {
+      status: 404,
+      statusText: 'Not found'
+    });
+  }
+
+  return new Response(JSON.stringify(product), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
