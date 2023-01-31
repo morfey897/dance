12d2ce1a5c33516247c3316dff9aac7c00c52f97
Maksym Cherniavskyi <morfey897@gmail.com>
2023-01-31 13:01:00 +0200

    Fixed

--- src/pages/api/data.ts
+++ src/pages/api/data.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from 'astro';
 
-export const get: APIRoute = () => {
-  const serverUrl = import.meta.env.DEFAULT_LANG;
+export const get: APIRoute = (params) => {
+  const serverUrl = JSON.stringify(params);//import.meta.env.DEFAULT_LANG;
   return {
     body: JSON.stringify({
       message: "This was a GET!: " + serverUrl
