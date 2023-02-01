de5b16580693a4c33802690b81c55c828bcd3d2c
Maksym Cherniavskyi <morfey897@gmail.com>
2023-02-01 19:53:50 +0200

    Added KV

--- src/pages/api/data.ts
+++ src/pages/api/data.ts
@@ -1,11 +1,11 @@
 import { getRuntime } from "@astrojs/cloudflare/runtime";
 
-export async function get(params) {
+export async function get(params, ctx) {
   const runtime = getRuntime(params.request);
   const lang = runtime?.env['DEFAULT_LANG'];
   // 
   // console.log('TEST');
-  const product = { lang: 'lang', rn: typeof runtime, loc: lang };//await getProduct(id);
+  const product = { lang: 'lang', rn: {...runtime}, loc: lang };//await getProduct(id);
 
   if (!product) {
     return new Response(null, {
