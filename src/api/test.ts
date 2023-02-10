0d9d96a8be08c218281f04d0b8ae2ec0274d4159
Maksym Cherniavskyi <morfey897@gmail.com>
2023-02-10 11:45:57 +0200

    Added catch

--- src/api/test.ts
+++ src/api/test.ts
@@ -3,7 +3,12 @@ import { env_PROXY_API_ARL } from "../utils/env";
 export async function get(request: Request) {
   const proxy = env_PROXY_API_ARL(request);
   const url = `${proxy.replace(/\/+$/, "")}/api/test`;
-  let response: Response = await fetch(url);
-  let data = await response.json();
+
+  let data = await new Promise((res, rej) => {
+    fetch(url)
+      .then((response: Response) => response.json())
+      .then(data => res(data))
+      .catch((e: Error) => res(e.message));
+  });
   return { data, url };
 }
