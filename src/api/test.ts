cab38c802b56891015379631b2269bd518cd8f64
Maksym Cherniavskyi <morfey897@gmail.com>
2023-02-10 11:42:22 +0200

    TEST last

--- src/api/test.ts
+++ src/api/test.ts
@@ -3,7 +3,7 @@ import { env_PROXY_API_ARL } from "../utils/env";
 export async function get(request: Request) {
   const proxy = env_PROXY_API_ARL(request);
   const url = `${proxy.replace(/\/+$/, "")}/api/test`;
-  // let response: Response = await fetch(url);
-  // let data = await response.json();
-  return { data: 'FETCH', url };
+  let response: Response = await fetch(url);
+  let data = await response.json();
+  return { data, url };
 }
