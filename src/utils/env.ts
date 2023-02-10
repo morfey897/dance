e5108a2f0143ce7c273d854b466aca733396c865
Maksym Cherniavskyi <morfey897@gmail.com>
2023-02-10 11:42:22 +0200

    TEST last

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,5 +3,5 @@ import { getRuntime } from "@astrojs/cloudflare/runtime";
 export function env_PROXY_API_ARL(request: Request) {
   const runtime = getRuntime(request);
   const value: string = runtime?.env['PROXY_API_ARL'] || import.meta.env.PROXY_API_ARL;
-  return `${value} && rn = ${JSON.stringify(runtime)}`;
+  return value;
 } 
