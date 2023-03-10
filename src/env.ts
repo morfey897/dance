ef9b7a773c8eb65d206172df2aeef3ace85abb20
Maksym Cherniavskyi <morfey897@gmail.com>
2023-03-10 15:18:21 +0200

    TTTT

--- src/env.ts
+++ src/env.ts
@@ -3,12 +3,16 @@ interface ImportMetaEnv {
   readonly GOOGLE_API_KEY: string;
 }
 
-const _ENV: ImportMetaEnv = JSON.parse(JSON.stringify(process?.env || import.meta.env));
+//  || import.meta.env
+const _ENV: ImportMetaEnv = JSON.parse(JSON.stringify(process?.env));
+console.log('DEPLOYMENT', _ENV);
 
 export const LANGS: Array<string> = (_ENV.LANGS || "").split(",").filter(lang => !!lang);
-export const GOOGLE_API_KEY = _ENV.GOOGLE_API_KEY;
+export const GOOGLE_API_KEY: string = _ENV.GOOGLE_API_KEY || "";
 
 export default {
+  str_f: JSON.stringify(process?.env),
+  _ENV,
   LANGS,
   GOOGLE_API_KEY
 }
