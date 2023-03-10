ee84bb87c78474ed515babfaf8a68a9a17a4ffa5
Maksym Cherniavskyi <morfey897@gmail.com>
2023-03-10 15:21:32 +0200

    ENV_TEST

--- src/env.ts
+++ src/env.ts
@@ -4,14 +4,14 @@ interface ImportMetaEnv {
 }
 
 //  || import.meta.env
-const _ENV: ImportMetaEnv = JSON.parse(JSON.stringify(process?.env));
+const _ENV: ImportMetaEnv = JSON.parse(JSON.stringify(process.env));
 console.log('DEPLOYMENT', _ENV);
 
 export const LANGS: Array<string> = (_ENV.LANGS || "").split(",").filter(lang => !!lang);
 export const GOOGLE_API_KEY: string = _ENV.GOOGLE_API_KEY || "";
 
 export default {
-  str_f: JSON.stringify(process?.env),
+  str_f: JSON.stringify(process.env),
   _ENV,
   LANGS,
   GOOGLE_API_KEY
