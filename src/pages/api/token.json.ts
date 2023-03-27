46c750fd7507241a08ad32e76229fca5a7bd77f2
Maksym Cherniavskyi <morfey897@gmail.com>
2023-03-27 19:00:00 +0300

    Test

---
+++ src/pages/api/token.json.ts
@@ -0,0 +1,48 @@
+import jwt from '@tsndr/cloudflare-worker-jwt'
+import type { APIRoute } from 'astro';
+import { getGoogleServiceAddress, getGoogleCalendarId } from "../../data/env";
+
+type CredentialType = {
+  type: string;
+  project_id: string;
+  private_key_id: string;
+  private_key: string;
+  client_email: string;
+  client_id: string;
+  auth_uri: string;
+  token_uri: string;
+};
+
+
+const SCOPES = [
+  'https://www.googleapis.com/auth/calendar',
+  'https://www.googleapis.com/auth/calendar.events',
+];
+
+export const get: APIRoute = async ({ params, request }) => {
+  const credentinal = getGoogleServiceAddress(request);
+  const header = {
+    alg: 'RS256',
+    typ: 'JWT',
+    kid: credentinal.private_key_id,
+  };
+  const iat = Math.floor(Date.now() / 1000);
+  const exp = iat + 3600;
+  const payload = {
+    iss: credentinal.client_email,
+    sub: credentinal.client_email,
+    iat,
+    exp,
+    scope: Array.isArray(SCOPES) ? SCOPES.join(' ') : SCOPES,
+    aud: credentinal.token_uri,
+    //"https://www.googleapis.com/oauth2/v4/token"
+  }
+
+  const token = await jwt.sign(payload, credentinal.private_key, { header });
+
+  return new Response(token, {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
